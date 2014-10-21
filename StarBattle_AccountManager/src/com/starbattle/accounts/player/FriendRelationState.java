c86cff11cccd62314631fc5cb22a10028944eabb
Rolleander <roll2@web.de>
2014-10-21 13:35:05 +0200

    Friend Relation State Fix

--- StarBattle_AccountManager/src/com/starbattle/accounts/player/FriendRelationState.java
+++ StarBattle_AccountManager/src/com/starbattle/accounts/player/FriendRelationState.java
@@ -2,8 +2,17 @@ package com.starbattle.accounts.player;
 
 public enum FriendRelationState {
 
-	Friends, Pending;   
+	Friends, Pending, Request;   
 
+	/**
+	 * This is the relation from the view of the player.
+	 * 
+	 * Friends = your friends
+	 * Pending = this relation is a friend request sent by you 
+	 * Request = this relation is a friend request from another player you have to decline or accept
+	 * 
+	 */
+	
 	FriendRelationState() {
 
 	}
