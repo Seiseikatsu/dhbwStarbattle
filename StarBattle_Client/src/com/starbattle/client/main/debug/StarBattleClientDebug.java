f701ae1bafbdf0abc9a238f8accccde58d15e0dc
Sebastian <sebastianmagin@yahoo.de>
2014-10-23 14:06:44 +0200

    Lobby View -> LoginView

--- StarBattle_Client/src/com/starbattle/client/main/debug/StarBattleClientDebug.java
+++ StarBattle_Client/src/com/starbattle/client/main/debug/StarBattleClientDebug.java
@@ -27,7 +27,7 @@ public class StarBattleClientDebug {
 		window.addView(new LobbyView(debugConnection));
 		
 		//set starting view
-		window.open(LobbyView.VIEW_ID);
+		window.open(LoginView.VIEW_ID);
 		
 	}
 	
